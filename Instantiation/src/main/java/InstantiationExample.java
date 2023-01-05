b350fe7a8f9aa9fe9fd14cdd5940de8d3f68499c
Brittany Glaze <niftybrittany@gmail.com>
2023-01-05 19:34:38 +0000

    Saved progress on Instantiation 2023-01-05 19:34:38 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,9 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+       
+            Object obj1 = new Object();
+            return obj1;
+    
     }
 }
