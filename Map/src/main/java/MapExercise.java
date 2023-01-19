015cd5032813cedfd173d24311e17d2c03b83881
Brittany Glaze <niftybrittany@gmail.com>
2023-01-19 15:07:03 +0000

    Saved progress on Sets 2023-01-19 15:07:03 +0000

--- Map/src/main/java/MapExercise.java
+++ Map/src/main/java/MapExercise.java
@@ -1,5 +1,6 @@
 
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Maps are key/value pairs. They are used in situations where we have some value that we'd like
@@ -26,7 +27,8 @@ public class MapExercise {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+        Map<Integer, String> map = new HashMap<>();
+        return map;
     }
 
     /**
@@ -35,8 +37,8 @@ public class MapExercise {
      * @return the size of map.
      */
     public int getSize(Map<Integer,String> map){
-//        return total number of key/value pairs in the map
-        return 0;
+        map = new HashMap<Integer, String>();
+        return map.size();
     }
 
     /**
@@ -47,6 +49,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+   
     }
 
     /**
@@ -66,6 +69,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
     }
 
     /**
