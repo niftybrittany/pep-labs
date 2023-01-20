7342baa6b99ceb1cdc4e24214a8638ba0c576039
Brittany Glaze <niftybrittany@gmail.com>
2023-01-20 00:00:46 +0000

    Saved progress on BankAccount 2023-01-20 00:00:46 +0000

--- BankAccount/src/main/java/BankService.java
+++ BankAccount/src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        balance = this.balance + amount;
     }
 
     /**
@@ -32,7 +32,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        
+        if((balance - amount) >= 0 ) {
+           balance = balance - amount;
+        }
+        
     }
 
     /**
@@ -40,6 +44,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
