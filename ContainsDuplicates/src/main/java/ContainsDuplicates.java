2dfdae67373958bbd1eebc65df4a423046f16382
Brittany Glaze <niftybrittany@gmail.com>
2023-02-23 19:17:10 +0000

    Saved progress on ContainsDuplicates 2023-02-23 19:17:10 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,4 @@
-
+import java.util.HashSet;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +8,16 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
-        return false;
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num: nums) {
+            if(set.contains(num)){
+                return true;
+            }else{
+            set.add(num); 
+            }
+        }
+        
+        return false;  
     }
 }
