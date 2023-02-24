9e93075c51b506f6884657bc895e162a11795c70
Brittany Glaze <niftybrittany@gmail.com>
2023-02-24 05:14:50 +0000

    Saved progress on InsertPosition 2023-02-24 05:14:50 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -17,8 +17,12 @@ public class InsertPosition {
     public int searchInsert(int[] nums, int target) {
        int index = -1;
         for(int i = 0; i<nums.length;i++){
-            if(target < nums[i] && target > nums[i-1]){
+            if(nums.length==1 && target < nums[i]){
                 index = i;
+            } else if (target < nums[i] && target > nums[i-1]){
+                index = i;
+            }else if (target > nums[i]){
+                index = i+1;
             }
         }
         return index;
