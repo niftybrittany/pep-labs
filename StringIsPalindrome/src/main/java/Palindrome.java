2d5145bfbc7596de1464cbfd3e26521f5d950db3
Brittany Glaze <niftybrittany@gmail.com>
2023-02-26 17:58:47 +0000

    Saved progress on SubArray 2023-02-26 17:58:47 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,11 +12,8 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        String reverse = "";
         boolean isPal = false;
-        for (int i = str.length() - 1; i>=0;i--){
-            reverse = reverse + str.charAt(i);
-        }
+        String reverse = new StringBuilder(str).reverse().toString();
         if(str.equals(reverse)){
             isPal = true;
         }
