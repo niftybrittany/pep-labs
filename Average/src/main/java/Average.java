cbfc1c9aee517b679ebb2a0e4e561f47d43814a2
Brittany Glaze <niftybrittany@gmail.com>
2023-02-27 00:19:29 +0000

    Saved progress on GetMax 2023-02-27 00:19:29 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -19,7 +19,7 @@ public class Average {
      */
     public double getAvg(int[] nums){
         double sum = 0;
-
+        
         for(int i=0; i<nums.length; i++){
         	sum = sum + nums[i];
         }
